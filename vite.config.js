899ff1c370f8c96943e360731edfc2fd7fc0f7f3
aurarieska <aurdiunair@gmail.com>
2026-07-15 14:54:28 +0700

    coba git pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/434241051_Aura-Rieska-Maharani_UAS-Workshop-UI-UX-Praktikum/',
   server: {
     port: 5173,
     open: true,
