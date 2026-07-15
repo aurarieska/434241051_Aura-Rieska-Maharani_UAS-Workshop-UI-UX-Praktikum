d7d67b5355b243c20b6d5ff8f5fc5e45fbf2c745
aurarieska <aurdiunair@gmail.com>
2026-07-15 15:06:37 +0700

    netlify deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/434241051_Aura-Rieska-Maharani_UAS-Workshop-UI-UX-Praktikum/',
   server: {
     port: 5173,
     open: true,
